b0654df30f4d25a732b17d2eb03af2fe9946d734
Arian Cayton <ariancayton@gmail.com>
2022-01-19 22:41:32 +0800

    add: library in local storage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import { readBooksFromDB } from './helper.js'
 import Book from './classes/Book.js'
 
-let myLibrary = [];
+const localStorageLibrary = JSON.parse(localStorage.getItem('myLibrary'))
+
+let myLibrary = localStorageLibrary !== null ? localStorageLibrary : await addBooksFromDBToLibrary();
+console.log(myLibrary);
+updateLocalStorage()
+renderBooks();
 
 
 async function addBooksFromDBToLibrary() {
    const books = await readBooksFromDB();
+   let bookArray = []
    for (let i = 0; i < books.length; i++) {
       const newBook = new Book(
          books[i].id,
@@ -15,8 +21,10 @@ async function addBooksFromDBToLibrary() {
          books[i].pageCount,
          books[i].pageCompleted
       )
-      myLibrary.push(newBook)
+      bookArray.push(newBook)
    }
+   console.log(typeof bookArray);
+   return bookArray
 }
 
 function addBookFromInputFormToLibrary(newBook) {
@@ -42,6 +50,15 @@ async function renderBooks() {
    }
 }
 
+function updateLocalStorage() {
+   localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+}
+
+/**
+ * Create a star container that contains the star rating of the book
+ * @param {*} stars - number of star ratings
+ * @returns 
+ */
 async function createStarsContainer(stars) {
    let starsContainer = document.createElement('div')
    starsContainer.classList.add('stars-container')
@@ -51,16 +68,73 @@ async function createStarsContainer(stars) {
    for (let i = 1; i < 5 + 1; i++) {
       var star = document.createElement('span')
       if (i <= stars) {
-         star.classList.add('fa', 'fa-star', 'checked')
+         star.classList.add('fas', 'fa-star', 'checked')
       } else {
-         star.classList.add('fa', 'fa-star')
+         star.classList.add('fas', 'fa-star')
       }
       starsContainer.appendChild(star)
    }
 
    return starsContainer.outerHTML
 }
+/** 
+ * Append newly added book to the grid container
+ *  
+*/
+async function renderNewBook(book) {
+   const bookContainer = document.createElement('div')
+   bookContainer.classList.add('book-card')
+   bookContainer.innerHTML = `
+      <h3 class="book-title">${book.title}</h3>
+      <h4 class="book-author">${book.author}</h4>
+      ${await createStarsContainer(book.stars)}
+      <span class="book-pageCount">
+      ${book.pageCount} Pages
+      </span>
+      `
+   $('.grid-container').append(
+      bookContainer
+   )
+}
 
 
-await addBooksFromDBToLibrary();
-renderBooks();
+$(document).ready(function () {
+   $('#add-book-card').click(function () {
+      $('.modal-container').addClass('show-modal');
+
+   })
+
+   $('#close').click(function () {
+      $('.modal-container').removeClass('show-modal');
+   })
+
+   $('#modal-form').submit(function (e) {
+
+      const title = $('#title-input').val()
+      const author = $('#author-input').val();
+      let stars = $('.modal-star-container input[type=radio]:checked').attr('id')
+      stars = parseInt(stars[stars.length - 1])
+      stars = (5 - stars) + 1;
+      const pageCount = parseInt($('#pageCount').val())
+      const pageCompleted = parseInt($('#pageCompleted').val())
+      if (pageCompleted > pageCount) {
+         alert("Page completed should be less than book's page count!")
+         return false;
+      }
+      const newBook = new Book(
+         0,
+         title,
+         author,
+         stars,
+         pageCount,
+         pageCompleted
+      )
+      alert("New Book Added!")
+      addBookFromInputFormToLibrary(newBook)
+      renderNewBook(newBook)
+      updateLocalStorage()
+      //Close the modal
+      $('.modal-container').removeClass('show-modal');
+      return false;
+   })
+})
